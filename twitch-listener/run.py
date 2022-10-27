7a0a6d054110263ff33d0fb74153d553f961e831
daltonv297 <daltonv297@gmail.com>
2022-10-26 17:18:56 -0700

    Added prelim csv data

--- twitch-listener/run.py
+++ twitch-listener/run.py
@@ -7,12 +7,12 @@ bot = listener.connect_twitch('mrbelms',
 
 # List of channels to connect to
 #channels_to_listen_to = ['xQc', 'KaiCenat', 'shroud', 'Amouranth', 'AdinRoss', 'loltyler1', 'Wirtual', 'Pestily', 'HasanAbi', 'moistcr1tikal', 'erobb221', 'tarik', 'pokimane', 'sodapoppin', 'NICKMERCS', 'summit1g', 'Loserfruit', 'LIRIK', 'Anomaly', 'Fextralife', 'Tfue', 'Castro_1021', 'Heelmike', 'tommyinnit']
-channels_to_listen_to = ['shroud', 'moistcr1tikal']
+channels_to_listen_to = ['shroud', 'moistcr1tikal', 'loltyler1', 'erobb221', 'tarik']
 # Scrape live chat data into raw log files. (Duration is seconds)
-bot.listen(channels_to_listen_to, duration = 10) 
+bot.listen(channels_to_listen_to, duration = 5000) 
 
 # Convert log files into .CSV format
 bot.parse_logs(timestamp = True)
 
 # Generate adjacency matrix
-bot.adj_matrix(weighted = True, matrix_name = "streamer_network.csv")
+#bot.adj_matrix(weighted = True, matrix_name = "streamer_network.csv")
